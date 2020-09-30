0761f69b4fd510c37122c3023697f78dd47e859b
Bruno Martins <brunorodrigues929@gmail.com>
2020-09-29 21:23:38 -0300

    Complete

--- src/index.js
+++ src/index.js
@@ -1,30 +1,9 @@
-const { request, response } = require("express");
 const express = require("express");
-const fs = require("fs");
-const { addListener } = require("process");
 const connection = require("./db/connection");
 const app = express();
 
 app.use(express.json());
 
-function reestartTables() {
-
-  connection.connect(function (err) {
-    if (err) {
-      console.error(`Database connection failed:${err.stack}`);
-      return console.log("erro na conexão");
-    }
-  });
-
-  connection.query(`DROP DATABASE IF EXISTS todolist;`, function (err, result) {
-    if (err) {
-      console.error('erro ao apagar list: ' + err.stack);
-      return;
-    }
-    console.log("drop database");
-    // console.log(result);
-  }).end();
-}
 
 function createDB() {
 
@@ -74,7 +53,7 @@ function addLogin(email, password) {
   });
 }
 
-async function removeLogin(email, password) {
+function removeLogin(email, password) {
   connection.connect(function (err) {
     if (err) {
       console.error(`Database connection failed:${err.stack}`);
@@ -83,89 +62,31 @@ async function removeLogin(email, password) {
   });
 
 
-
-
-  await connection.query(`DELETE FROM todolist.login WHERE id = '${result[0].id}';`, function (err, result) {
+  connection.query(`DELETE FROM todolist.login WHERE email = '${email}';`, function (err, result) {
     if (err) {
       return console.error("Erro ao remover login");
     }
     console.log("login removido!");
   })
-
-
-
-
 }
 
-function listLogin() {
-  connection.connect(function (err) {
-    if (err) {
-      console.error(`Database connection failed:${err.stack}`);
-      return console.log("erro na conexão");
-    }
-  });
+function findLoginWithEmail(email) {
 
-  connection.query("SELECT * FROM todolist.login ;", function (err, result) {
-    if (err) {
-      return console.error("Erro ao logins não encontrados");
-    }
-    console.log(result);
-  })
-}
-
-function updateLogin(id, email, senha) {
-  connection.connect(function (err) {
-    if (err) {
-      console.error(`Database connection failed:${err.stack}`);
-      return console.log("erro na conexão");
-    }
-  });
-
-  connection.query("UPDATE todolist.login SET email='novoEmail', senha='novaSenha' WHERE id=2 ;", function (err, result) {
-    if (err) {
-      return console.error("Erro ao atualizar login");
-    }
-    console.log("login atualizado!");
-    console.log(result);
-  })
-}
-
-async function findIdWithLogin(email) {
-  await connection.connect(function (err) {
-    if (err) {
-      console.error(`Database connection failed:${err.stack}`);
-      return console.log("erro na conexão");
-    }
-
-  });
   return new Promise((resolve, reject) => {
     connection.query(`SELECT * FROM todolist.login WHERE email = '${email}';`, function (err, result) {
       if (err) {
         reject(err)
-        // callback(err, null);
       }
-
-      // console.log(result[0].id);
-      resolve(result[0].id)
-      // console.log(result);
-      // return result[0].id;
+      resolve(result[0]);
     })
   })
 }
 
-
 // ------ FUNÇÕES DE TODO
 
-function addTodo(idLogin, title, status) {
-  connection.connect(function (err) {
-    if (err) {
-      console.error(`Database connection failed:${err.stack}`);
-      return console.log("erro na conexão");
-    }
-  });
-  let stringSQL =
-    "INSERT INTO todolist.list (idLogin, title, status) VALUES (" + idLogin + "," + title + "," + status + ");";
-  //connection.query(`INSERT INTO todolist.list (idLogin, title, status) VALUES (${idLogin},${title},${status});`, function(err, result){
+function addTodo(idLogin, title) {
+  let stringSQL = `INSERT INTO todolist.list (idLogin, title) VALUES ('${idLogin}','${title}');`;
+
   connection.query(stringSQL, function (err, result) {
     if (err) {
       return console.error("Erro ao adicionar todo");
@@ -176,19 +97,11 @@ function addTodo(idLogin, title, status) {
 }
 
 function removeTodo(idTodo) {
-  connection.connect(function (err) {
-    if (err) {
-      console.error(`Database connection failed:${err.stack}`);
-      return console.log("erro na conexão");
-    }
-  });
-
-  connection.query("DELETE FROM todolist.list WHERE id = 1;", function (err, result) {
+  connection.query(`DELETE FROM todolist.list WHERE id = '${idTodo}';`, function (err, result) {
     if (err) {
       return console.error("Erro ao remover todo");
     }
     console.log("todo removido!");
-
   })
 }
 
@@ -209,154 +122,152 @@ function listTodo() {
   })
 }
 
-function updateTodo(id, title, status) {
-  connection.connect(function (err) {
-    if (err) {
-      console.error(`Database connection failed:${err.stack}`);
-      return console.log("erro na conexão");
-    }
-  });
+function updateTodo(title) {
+  // connection.connect(function (err) {
+  //   if (err) {
+  //     console.error(`Database connection failed:${err.stack}`);
+  //     return console.log("erro na conexão");
+  //   }
+  // });
 
-  connection.query("UPDATE todolist.list SET title='atualizar', status=3 WHERE id=2 ;", function (err, result) {
+  connection.query(`SELECT * FROM todolist.list WHERE title = '${title}';`, function (err, result) {
     if (err) {
-      return console.error("Erro ao atualizar todo");
+      console.log("erro no select do todo")
+      reject(err)
     }
-    console.log("todo atualizado!");
-    console.log(result);
+
+    if(result[0].status===0){
+      connection.query(`UPDATE todolist.list SET status='1' WHERE title='${title}';`, function (err, result1) {
+        if (err) {
+          return console.error("Erro ao atualizar todo");
+        }
+        console.log("todo atualizado!");
+        
+      })
+    }else{
+      connection.query(`UPDATE todolist.list SET status='0' WHERE title='${title}';`, function (err, result1) {
+        if (err) {
+          return console.error("Erro ao atualizar todo");
+        }
+        console.log("todo atualizado!");
+      })
+    }
   })
+
+  
 }
 
-// reestartTables();
+function findTodoWithTitle(title) {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM todolist.list WHERE title = '${title}';`, function (err, result) {
+      if (err) {
+        console.log("erro no select do todo")
+        reject(err)
+      }
+      resolve(result[0]);
+    })
+  });
+}
+
+function findAllTodos() {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM todolist.list;`, function (err, result) {
+      if (err) {
+        console.log("erro no select do todo")
+        reject(err)
+      }
+
+      resolve(result);
+    })
+  });
+}
+
+
 // createDB();
-// addLogin('jorder','redroj');
-// removeLogin('user2', 'senha');
 
 
 
-var id;
-
-findIdWithLogin('user2').then(id => console.log("I am id:" + id));
-
-
-console.log(`#### ${id}`)
-
-// addTodo(1, "non", 2);
-// removeTodo(1);
-// listTodo();
-// updateTodo(2, 'atualizar', 8);
-
-//##########################################
-
-app.get('/teste', (request, response) => {
-  return response.json({ ok: true });
+app.post('/login', (request, response)=>{
+  const { email , password} = request.body;
+  addLogin(email, password);
+  return response.json({ message: "Cadastrado com sucesso!"});
 });
-
-
-
-// app.get('/login', (request, response)=>{
-//   //array  
-//   const logins = findAllLoigns();  
-//   return response.json([{id, email, password}...]);
-// })
-
-
-
-// app.post('/login', (request, response)=>{
-//   const { email , password} = request.body;
-//   addLogin(email, password);
-//   return response.json({ message: "Cadastrado com sucesso!"});
-// });
-
-
 
 app.delete('/login', (request, response) => {
   const { email, password } = request.body;
-  const passwordCheck = findPassWithLogin(email);
-  if (passwordCheck === password) {
-    removeLogin(email);
-    return response.json({ message: "Cadastrado removido com sucesso!" });
-  }
-  return response.json({ message: "Senha incorreta!" })
+  findLoginWithEmail(email).then(login=>{
+    if (login.password === password) {
+      removeLogin(email);
+      return response.json({ message: "Cadastrado removido com sucesso!" });
+    }
+    return response.json({ message: "Senha/Email incorreta!" })
+  });
+});
+
+app.post('/list', (request, response)=>{
+  const { email, password, title} = request.body;
+  findLoginWithEmail(email).then(login=>{
+    if (login.password === password) {
+      addTodo(login.id, title);
+      return response.json({
+        message: "Item adicionado a lista",
+        email: email,
+        title: title,
+        status: 0
+      });
+    }
+    return response.json({ message: "Senha/Email incorreta!" })
+  });
+});
+
+app.delete('/list', (request, response)=>{
+  const { email, password, title} = request.body;
+  
+  findLoginWithEmail(email).then(login=>{
+    if (login.password != password) {
+      return response.json({ message: "Senha/Email incorreta!" })
+    }
+  });
+
+  findTodoWithTitle(title).then(todo=>{
+    removeTodo(todo.id)
+    return response.json({
+      message: `Item ${todo.title} alterado a lista`,
+    });
+  });
+  
+});
+
+app.put('/alterStatus', (request, response)=>{
+  const { email, password, title} = request.body;
+  
+  findLoginWithEmail(email).then(login=>{
+    if (login.password != password) {
+      return response.json({ message: "Senha/Email incorreta!" })
+    }
+  });
+  
+  updateTodo(title);
+
+  findTodoWithTitle(title).then(todo=>{
+    return response.json({
+      message: `Status do item ${todo.title} atualizado!`
+    });
+  });
+  
 });
 
 
 
-
-
-// app.post('/list', (request, response)=>{
-//   const { email, title} = request.body;
-//   //verifica se email existe e retorna o id dele no banco
-//   const idLogged = verifyEmail(email);
-//   if (idLogged){
-//     // status no banco ta default 0, 0 nao feito,1 feito, addList cadastra
-//     addList(idLogged, title);
-//     return response.json({
-//       message: "Item adicionado a lista",
-//       email: email,
-//       title: title,
-//       status: 0
-//     });
-//   }
-//   return response.json({message: "Email inexistente!"});
-// });
-
-
-
-// app.delete('/list', (request, response)=>{
-//   const { email, title} = request.body;
-//   //verifica se email existe e retorna o id dele no banco
-//   const idLogged = verifyEmail(email);
-//   if (idLogged){
-//     // status no banco ta default 0, 0 nao feito,1 feito, addList cadastra
-//     addList(idLogged, title);
-//     return response.json({
-//       message: "Item adicionado a lista",
-//       email: email,
-//       title: title,
-//       status: 0
-//     });
-//   }
-//   return response.json({message: "Email inexistente!"});
-// });
-
-
-
-// app.delete('/list', (request, response)=>{
-//   const { email , password, title} = request.body;
-//     const passwordCheck = findPassWithLogin(email);
-//     if (passwordCheck === password){
-//       const item = findListWithTitle(title);
-//       removeList(item.id);
-//       return response.json({ message: "Item removido com sucesso!" });
-//     }
-//     return response.json({message: "Senha incorreta!"})
-// });
-
-
-
-// app.put('/alterStatus', (request, response)=>{
-//   const { email , password, title} = request.body;
-//     const passwordCheck = findPassWithLogin(email);
-//     if (passwordCheck === password){
-//       const item = findListWithTitle(title);
-//       const newStatus = alterStatus(item.id);// status = !status // 1 ? 0 : 1
-//       return response.json({
-//         message: "Item alterado na lista",
-//         email: email,
-//         title: title,
-//         status: newStatus
-//       });
-//     }
-//     return response.json({message: "Senha incorreta!"})
-// });
-
-
-
-// app.get('/list', (request, response)=>{
-//   //array  
-//   const logins = findAllList();  
-//   return response.json([{id, email, title, status}...]);
-// });
+app.get('/list', (request, response)=>{
+  findAllTodos().then(items=>{
+    const array = [];
+    items.map(i => {
+      array.push({id: i.id, idLogin: i.idLogin, title: i.title, status: i.status})
+    })
+    return response.json(array);
+  });  
+});
 
 
 
